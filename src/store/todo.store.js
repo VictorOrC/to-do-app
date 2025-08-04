7a9639e76e6ac572eee4dfdae87b57e998eacf2e
Victor Orduno <vict.rcastan.s@gmail.com>
2025-08-04 08:48:01 -0700

    Inicializar metodos que se ocuparan para el store

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -20,6 +20,51 @@ const initStore = () => {
     console.log('InitStore Papu');
 }
 
+const loadStore = () => {
+    throw new Error('Not implemented');
+}
+
+const addTodo = ( description ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Funcion para marvar como acompletado todo
+ * @param {String} todoId identificador de todo
+ */
+const toggleTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Funcion para eliminar todo
+ * @param {String} todoId identificador de todo 
+ */
+const deleteTodo = ( todoId ) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * Funcion para eliminar los todos ya completados
+ */
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = ( newFilter = Filters.All ) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
+    deleteCompleted,
+    deleteTodo,
+    getCurrentFilter,
     initStore,
+    loadStore,
+    setFilter,
+    toggleTodo,
 }
